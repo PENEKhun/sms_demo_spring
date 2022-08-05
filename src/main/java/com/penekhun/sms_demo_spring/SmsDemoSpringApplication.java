bd87eb85b2a7d4d7435a3f0bf4e6522d826064aa
PENEKhun <penekhun@gmail.com>
2022-08-05 13:50:49 +0900

    Initial Commit

---
+++ src/main/java/com/penekhun/sms_demo_spring/SmsDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.penekhun.sms_demo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsDemoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsDemoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/penekhun/sms_demo_spring/SmsDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penekhun.sms_demo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsDemoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
